1cbdc044cff516138a3d7570e3017c19b2d42569
kanishk-gupta <kanishkgupta55@gmail.com>
2025-06-02 01:02:21 +0530

    Made changes for vercel deployment

--- index.js
+++ index.js
@@ -9,44 +9,35 @@ const corsOptions = require('./src/corsConfig.js');
 const { ErrorHandler, handleError, responseHandler } = require('./src/helpers');
 const { userUnrestricted, userRestricted } = require('./src/routes');
 
-// function main() {
-    const app = express();
-    app.use(helmet());
+const app = express();
+app.use(helmet());
 
-    app.use(express.json({ limit: "400mb" }));
-    app.use(express.urlencoded({ limit: "400mb", extended: false }));
+app.use(express.json({ limit: "400mb" }));
+app.use(express.urlencoded({ limit: "400mb", extended: false }));
 
-    app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-    // Apply the response handler middleware to all routes
-    app.use(responseHandler);
+// Apply the response handler middleware to all routes
+app.use(responseHandler);
 
-	app.use('/', userUnrestricted); // Unrestricted routing file
-	app.use('/', userRestricted); // Restricted routing file
+app.use('/', userUnrestricted); // Unrestricted routing file
+app.use('/', userRestricted); // Restricted routing file
 
 
-    // Handle unknown routes
-    app.all("/*splat", (req, res, next) => {
-        next(new ErrorHandler(`Can't find ${req.originalUrl} on this server!`, 404));
+// Handle unknown routes
+app.all("/*splat", (req, res, next) => {
+    next(new ErrorHandler(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Always use the end of other middlewares and routes for it to function correctly
+app.use((err, req, res, next) => {
+    handleError(err, res);
+});
+
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`API server started on: ${PORT}`);
     });
+}
 
-    // Always use the end of other middlewares and routes for it to function correctly
-    app.use((err, req, res, next) => {
-        handleError(err, res);
-    });
-
-    if (require.main === module) {
-        app.listen(PORT, () => {
-            console.log(`API server started on: ${PORT}`);
-        });
-    }
-
-
-    // app.listen(PORT, () => {
-    //     console.log(`API server started on: ${PORT}`);
-    // });
-
-//     return app;
-// };
-// const mainApp = main();
 module.exports = app;
